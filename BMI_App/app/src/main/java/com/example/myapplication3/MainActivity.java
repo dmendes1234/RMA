098e95449c95bc31623d197f9d4eb7058f3df05a
dmendes1234 <dmendes@etfos.hr>
2021-03-29 22:23:03 +0200

    Update BMI_App

--- BMI_App/app/src/main/java/com/example/myapplication3/MainActivity.java
+++ BMI_App/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static String TAG = "Done with: ";
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText etHeight;
     private Button bCalculateBmi;
     private TextView tvBmiResult;
+    private TextView tvBmiResultTitle;
+    private TextView tvBmiResultDescription;
+    private ImageView imageBmiResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etHeight = findViewById(R.id.etHeight);
         bCalculateBmi = findViewById(R.id.bCalculateBmi);
         tvBmiResult = findViewById(R.id.tvBmiResult);
+        tvBmiResultTitle = findViewById(R.id.tvBmiResultTitle);
+        tvBmiResultDescription = findViewById(R.id.tvBmiResultDescription);
+        imageBmiResult = findViewById(R.id.imageBmiResult);
 
         bCalculateBmi.setOnClickListener(this);
         Log.d(TAG, "Setting view");
@@ -42,9 +50,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         double weight = Double.parseDouble(weightString);
         double height = Double.parseDouble(heightString);
 
-        double result = calculateBmi(weight, height);
+        if(weight == 0 || height == 0 || weight > 350 || height > 2.5) {
+            Toast.makeText(this, "Pogrešan unos!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            double result = calculateBmi(weight, height);
+            tvBmiResult.setText("Your BMI is " + String.format("%.2f", result));
 
-        tvBmiResult.setText(String.format("%.2f", result));
+            if (result < 18.5) {
+                tvBmiResultTitle.setText("You are underweight!");
+                tvBmiResultDescription.setText("People who are underweight may need to eat more often to consume enough calories to gain weight. " +
+                        "This is especially true if you don't have a very big appetite. " +
+                        "Aim for six small meals spread throughout the day instead of trying to pack more into three meals.");
+                imageBmiResult.setImageResource(R.drawable.underweight);
+            } else if (result > 24.9) {
+                tvBmiResultTitle.setText("You are overweight!");
+                tvBmiResultDescription.setText("Consume nutrient-dense foods, " +
+                        "such as lean protein, whole grains, legumes, low-fat dairy products, nuts and seeds and fruits and vegetables. " +
+                        "Limit foods that are high in saturated fat, cholesterol, sugars and sodium, as these can increase your risks for certain health conditions.");
+                imageBmiResult.setImageResource(R.drawable.overweight);
+            } else {
+                tvBmiResultTitle.setText("You are normal weight!");
+                tvBmiResultDescription.setText("You are good. Keep it up!");
+                imageBmiResult.setImageResource(R.drawable.normal_weight);
+            }
+        }
     }
 
     private double calculateBmi(double weight, double height){
